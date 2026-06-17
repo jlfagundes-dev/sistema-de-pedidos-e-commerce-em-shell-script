3ba1566c9feacf2e75fa351b3345a2df4c06fb42
jlfagundes-dev <jlfagundes@gmail.com>
2026-06-17 20:54:05 -0300

    feat: fluxo de registro produto no csv com id incremental

---
+++ ecommerce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variáveis
+PROXIMO_ID=1
+
+# Solicitar o usuário para digitar o nome do produto
+read -p "Digite o nome do produto: " PRODUTO
+
+# Criar arquivo csv com o cabeçalho se não existir
+if [ ! -f produtos.csv ]; then
+    echo "id_prod,desc_prod" > produtos.csv
+fi
+
+# Gravar o nome do produto em um arquivo csv
+echo "$PROXIMO_ID,$PRODUTO" >> produtos.csv
+TOTAL_PRODUTOS_CADASTRADOS=$(wc -l < produtos.csv)
+PROXIMO_ID=$TOTAL_PRODUTOS_CADASTRADOS # Não estou usando o +1 porque tem o cabeçalho
+
+# Exibir mensagem de sucesso
+echo "Produto '$PRODUTO' registrado com sucesso!"
