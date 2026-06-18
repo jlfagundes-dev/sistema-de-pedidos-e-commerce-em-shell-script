7e173453cb3f4580e85eefc5017c73771f2fcfff
jlfagundes-dev <jlfagundes@gmail.com>
2026-06-17 21:47:47 -0300

    fix: ajuste da logica de sanitizacao para tratar espacos em branco

--- ecommerce.sh
+++ ecommerce.sh
@@ -2,13 +2,14 @@
 
 # Variáveis
 PROXIMO_ID=0
+ARQUIVO="pedidos.csv"
 
 while true; do
     # Solicitar o usuário para digitar o nome do produto
     read -p "Digite o nome do produto: " PRODUTO
 
     # Validar input do usuário
-    if [ -z "$PRODUTO" ]; then # -z verifica se a string é vazia
+    if [ -z "${PRODUTO// /}" ]; then # -z verifica se a string é vazia ou somente espaços
         echo "O nome do produto não pode ser vazio. Por favor, tente novamente."
     else
         break
@@ -16,14 +17,14 @@ while true; do
 done
     
 # Criar arquivo csv com o cabeçalho se não existir
-if [ ! -f produtos.csv ]; then
-    echo "NUMERO_PEDIDO,NOME_DO_PRODUTO" > produtos.csv
+if [ ! -f $ARQUIVO ]; then
+    echo "NUMERO_PEDIDO,NOME_DO_PRODUTO" > $ARQUIVO
 fi
 
 # Gravar o NUMERO_PEDIDO,NOME_DO_PRODUTO em um arquivo csv
-TOTAL_PRODUTOS_CADASTRADOS=$(($(wc -l < produtos.csv) - 1)) # Subtrai 1 para não contar o cabeçalho
+TOTAL_PRODUTOS_CADASTRADOS=$(($(wc -l < $ARQUIVO) - 1)) # Subtrai 1 para não contar o cabeçalho
 PROXIMO_ID=$((TOTAL_PRODUTOS_CADASTRADOS + 1)) # $(()) porque é uma expressão aritmética
-echo "$PROXIMO_ID,$PRODUTO" >> produtos.csv
+echo "$PROXIMO_ID,$PRODUTO" >> $ARQUIVO
 
 # Exibir mensagem de sucesso
 echo "Produto '$PRODUTO' registrado com sucesso!"
